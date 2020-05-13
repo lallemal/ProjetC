a7a2adf9264617baba4f1dfc160524869e4a0344
Robin BERTIN <robin.bertin@grenoble-inp.org>
2020-05-13 09:55:57 +0200

    update frame alloc func

--- src/ei_frameclass.c
+++ src/ei_frameclass.c
@@ -5,10 +5,15 @@
 * Created:          05/12/20
 * Description:      
 *****************************************************************************/
+#include <stdlib.h>
+
+
 #include "ei_widgetclass.h"
+#include "ei_widget.h"
 
 typedef struct ei_frame {
-	ei_widget_t*	widget;
+	ei_widget_t	widget;
+
 	ei_color_t	color;
 	int		border_width;
 	ei_relief_t	relief;
@@ -25,20 +30,9 @@ typedef struct ei_frame {
 
 void* ei_frame_allocfunc (void)
 {
-	size_t frame_attribute_size = 0;
-	frame_attribute_size += sizof(ei_widget_t *);
-	frame_attribute_size += sizeof(ei_color_t);  // color
-	frame_attribute_size += sizeof(int);  // border_width
-	frame_attribute_size += sizeof(ei_relief_t);  // relief
-	frame_attribute_size += sizeof(char *);  // text
-	frame_attribute_size += sizeof(ei_font_t);  // text_font
-	frame_attribute_size += sizeof(ei_anchor_t);  // text_anchor
-	frame_attribute_size += sizeof(ei_surface_t);  // img
-	frame_attribute_size += sizeof(ei_rect_t *);  // img_rect
-	frame_attribute_size += sizeof(ei_anchor_t);  // img_anchor
-	void *memoire_frame = malloc(frame_attribute_size);
-	for (void *i = memoire_frame; i < memoire_frame + frame_attribute_size;i++) {
-		*i = NULL;
+	ei_frame_t *newFrame = malloc(sizeof(ei_frame_t));
+	for (void *i = newFrame; i < newFrame + sizeof(ei_frame_t);i++) {
+		*i = 0;
 	}
-	return memoire_frame;
+	return newFrame;
 }
