28d14ded0e29d39b03aec1a13afcdac7a04dad91
Robin BERTIN <robin.bertin@grenoble-inp.org>
2020-05-14 14:30:03 +0200

    finish update pixel, add map rgba color

--- src/ei_draw.c
+++ src/ei_draw.c
@@ -16,6 +16,62 @@
 #define min(a,b) (a<=b?a:b)
 
 
+uint32_t		ei_map_rgba		(ei_surface_t surface, const ei_color_t* color)
+{
+	int ir, ig, ib, ia;
+	hw_surface_get_channel_indices(surface, &ir, &ig, &ib, &ia);
+	uint32_t color_hex = 0;
+	color_hex += color->red << (8*ir);
+	color_hex += color->green << (8*ig);
+	color_hex += color->blue << (8*ib);
+	if (ia != -1) {
+		color_hex += color->alpha << (8*ia);
+	}
+	return color_hex;
+}
+
+
+ei_color_t ei_map_color (ei_surface_t surface, const uint32_t rgba)
+{
+	int ir, ig, ib, ia;
+	hw_surface_get_channel_indices(surface, &ir, &ig, &ib, &ia);
+	ei_color_t color;
+	uint32_t mask = 255;
+	color.red = (rgba & (mask << 8*ir)) >> (8*ir);
+	color.blue = (rgba & (mask << 8*ib)) >> (8*ib);
+	color.green = (rgba & (mask << 8*ig)) >> (8*ig);
+	if (ia != -1) {
+		color.alpha = (rgba & (mask << 8*ia)) >> (8*ia);
+	}
+	else {
+		color.alpha = 0;
+	}
+
+}
+
+
+void update_pixel	(ei_surface_t destination,
+			uint32_t* dest_pt,
+			ei_surface_t source,
+			uint32_t* src_pt,
+			ei_bool_t alpha)
+{
+	if (alpha == EI_FALSE) {
+		*dest_pt = *src_pt;
+	}
+	else {
+		ei_color_t color_dest = ei_map_color(destination, *dest_pt);
+		ei_color_t color_src = ei_map_color(source, *src_pt);
+		ei_color_t color_alpha;
+		color_alpha.red = (color_src.alpha * color_src.red + (255 - color_src.alpha) * color_dest.red)/255;
+		color_alpha.blue = (color_src.alpha * color_src.blue + (255 - color_src.alpha) * color_dest.blue)/255;
+		color_alpha.green = (color_src.alpha * color_src.green + (255 - color_src.alpha) * color_dest.green)/255;
+		*dest_pt = ei_map_rgba(destination, &color_alpha);
+	}
+}
+
+
+
 ei_rect_t inter_rect(const ei_rect_t* rect1, const ei_rect_t* rect2)
 {
 	int x1_min = rect1->top_left.x;
@@ -72,10 +128,10 @@ int			ei_copy_surface		(ei_surface_t		destination,
 	for (int i=0; i< dst_rect->size.height; i++) {
 		hw_surface_set_origin(destination, ei_point(dest_x, dest_y+i));
 		hw_surface_set_origin(source, ei_point(src_x, src_y+i));
-		uint8_t* dest_pt = hw_surface_get_buffer(destination);
-		uint8_t* src_pt  = hw_surface_get_buffer(source);
+		uint32_t* dest_pt = (uint32_t *) hw_surface_get_buffer(destination);
+		uint32_t* src_pt  = (uint32_t *) hw_surface_get_buffer(source);
 		for (int j = 0; j<dst_rect->size.height; j++) {
-			update_pixel(destination, &dest_pt, destination, source, &src_pt, alpha);
+			update_pixel(destination, dest_pt, source, src_pt, alpha);
 			dest_pt++;
 			src_pt++;
 		}
