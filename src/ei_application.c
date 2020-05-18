2fba94e00d832b03365e9e575dd63d0a33d8a73a
Whayx <devriesere.aymeric@gmail.com>
2020-05-18 17:51:41 +0200

    remove commentary

--- src/ei_application.c
+++ src/ei_application.c
@@ -85,6 +85,7 @@ void ei_app_free(void)
 }
 
 
+
 void ei_app_quit_request(void)
 {
 	running = false;
@@ -102,10 +103,9 @@ void ei_app_run(void)
 		list_rect_head = NULL;
 		list_rect_tail = NULL;
 		rect_status = LIST_RECT_NORMAL;
-                ei_app_quit_request();
-//		if (event.type == ei_ev_keydown) {
-//			ei_app_quit_request();
-//		}
+		if (event.type == ei_ev_keydown) {
+			ei_app_quit_request();
+		}
 		hw_event_wait_next(&event);
 	}
 }
