0e56f04015874bc0ef67ec9fb74ee77bc701b5ef
Louise_llmnd <lallemand.louise@gmail.com>
2020-05-21 17:11:29 +0200

    problem of east anchor solved (the picture was on the left and visible)

--- tests/button.c
+++ tests/button.c
@@ -53,13 +53,14 @@ int main(int argc, char** argv)
 	char*		button_title		= "Mon premier Bouton !";
 	ei_anchor_t     button_anchor           = ei_anc_center;
 	ei_color_t	button_text_color	= {0x00, 0x00, 0x00, 0xff};
-	ei_anchor_t     button_text_anchor      = ei_anc_north;
+	ei_anchor_t     button_text_anchor      = ei_anc_northeast;
 	int		button_corner_radius	= 20;
 	ei_relief_t	button_relief		= ei_relief_raised;
 	int		button_border_width	= 6;
 	ei_callback_t	button_callback 	= button_press;
 	ei_surface_t    image;
 	ei_rect_t*      rect_img;
+	ei_anchor_t     anchor_image            = ei_anc_east;
 
 	/* Create the application and change the color of the background. */
 	ei_app_create(screen_size, EI_FALSE);
@@ -67,15 +68,15 @@ int main(int argc, char** argv)
 
 	/* Create, configure and place the button on screen. */
 	button = ei_widget_create("button", ei_app_root_widget(), NULL, NULL);
-        //image = hw_image_load("misc/klimt.jpg", ei_app_root_surface());
-        //ei_rect_t  rect = hw_surface_get_rect(image);
-        //rect_img->size.height = rect.size.height;
-        //rect_img->size.width = rect.size.width;
-        //rect_img->top_left.x = rect.top_left.x;
-        //rect_img->top_left.y = rect.top_left.y;
+        image = hw_image_load("misc/flag.png", ei_app_root_surface());
+        ei_rect_t  rect = hw_surface_get_rect(image);
+        rect_img->size.height = rect.size.height;
+        rect_img->size.width = rect.size.width;
+        rect_img->top_left.x = rect.top_left.x;
+        rect_img->top_left.y = rect.top_left.y;
 	ei_button_configure	(button, &button_size, &button_color,
-				 &button_border_width, &button_corner_radius, &button_relief, &button_title, NULL, &button_text_color, &button_text_anchor,
-				 NULL, NULL, NULL, &button_callback, NULL);
+				 &button_border_width, &button_corner_radius, &button_relief, NULL, NULL, NULL, NULL,
+				 &image, &rect_img, &anchor_image, &button_callback, NULL);
 	ei_place(button, &button_anchor, &button_x, &button_y, NULL, NULL, NULL, NULL, NULL, NULL );
 
 	/* Hook the keypress callback to the event. */
